f6c5b71cecaa6be803f1cadfe9069be9fc47dcbf
Jeffrey Charles <jeffreycharles@gmail.com>
2012-05-12 22:39:05 -0400

    Listen for orientation changes

--- src/com/beyondtechnicallycorrect/cowbell/CowbellActivity.java
+++ src/com/beyondtechnicallycorrect/cowbell/CowbellActivity.java
@@ -1,13 +1,25 @@
 package com.beyondtechnicallycorrect.cowbell;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Activity for ringing a cowbell
  */
-public class CowbellActivity extends Activity {
+public class CowbellActivity extends Activity implements SensorEventListener {
     
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private Sensor mMagneticField;
+	
+	private float[] mGravity;
+	private float[] mGeomagneticVector;
+	
 	/**
 	 * Called when the activity is first created
 	 */
@@ -15,5 +27,88 @@ public class CowbellActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        mSensorManager = (SensorManager)this.getSystemService(SENSOR_SERVICE);
+		mAccelerometer =
+				mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mMagneticField =
+				mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
     }
+
+    /**
+     * Called when the activity leaves the foreground
+     */
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(this);
+	}
+
+	/**
+	 * Called when the activity appears in the foreground
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mSensorManager.registerListener(
+				this,
+				mAccelerometer,
+				SensorManager.SENSOR_DELAY_UI
+			);
+		mSensorManager.registerListener(
+				this,
+				mMagneticField,
+				SensorManager.SENSOR_DELAY_UI
+			);
+	}
+
+	/**
+	 * Called when one of the sensor's accuracy changes
+	 */
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// Do nothing
+	}
+
+	/**
+	 * Called when a sensor event occurs
+	 */
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if(event.sensor == mAccelerometer) {
+			mGravity = event.values;
+		}
+		if(event.sensor == mMagneticField) {
+			mGeomagneticVector = event.values;
+		}
+		
+		if(mGravity == null || mGeomagneticVector == null) {
+			return;
+		}
+		
+		final int R_CAPACITY = 9;
+		float[] rRotationMatrix = new float[R_CAPACITY];
+		float[] iRotationMatrix = null;
+		boolean gotResult =
+				SensorManager.getRotationMatrix(
+						rRotationMatrix, // out variable
+						iRotationMatrix, // out variable
+						mGravity,
+						mGeomagneticVector
+					);
+		if(!gotResult) {
+			return;
+		}
+		
+		final int ORIENTATION_CAPACITY = 3;
+		float[] orientation = new float[ORIENTATION_CAPACITY];
+		SensorManager.getOrientation(rRotationMatrix, orientation);
+		
+		final int ROLL_INDEX = 2;
+		final float DEGREES_PER_RADIAN = 57.2957795f;
+		float roll = orientation[ROLL_INDEX] * DEGREES_PER_RADIAN;
+		Log.d("CowbellActivity", "Roll: " + roll);
+	}
+    
+    
 }
